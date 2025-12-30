cbd22c61d1f8e529787cfe48e2d98ee13f735722
mind <rioeyuzenin@gmail.com>
2025-12-30 22:42:13 +0700

    This

--- src/main/java/me/itsskeptical/displaytags/entities/ClientEntity.java
+++ src/main/java/me/itsskeptical/displaytags/entities/ClientEntity.java
@@ -2,78 +2,89 @@ package me.itsskeptical.displaytags.entities;
 
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.protocol.entity.data.EntityData;
-import com.github.retrooper.packetevents.protocol.entity.data.EntityDataTypes;
 import com.github.retrooper.packetevents.protocol.entity.type.EntityType;
+import com.github.retrooper.packetevents.wrapper.PacketWrapper;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerDestroyEntities;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityMetadata;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSetPassengers;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSpawnEntity;
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerUpdateEntityMetadata;
+import io.github.retrooper.packetevents.util.SpigotConversionUtil;
+import io.github.retrooper.packetevents.util.SpigotReflectionUtil;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.UUID;
 
-public abstract class ClientEntity {
+public class ClientEntity {
+    private final int entityId;
+    private final UUID uuid;
 
-    protected final int entityId;
-    protected final Location location;
-    protected final EntityType type;
-    protected final List<EntityData> metadata = new ArrayList<>();
+    private final EntityType type;
+    private Location location;
 
-    protected ClientEntity(EntityType type, Location location) {
-        this.entityId = ThreadLocalRandom.current().nextInt(1_000_000, Integer.MAX_VALUE);
-        this.type = type;
-        this.location = location.clone();
+    public ClientEntity(org.bukkit.entity.EntityType type, Location location) {
+        this.entityId = SpigotReflectionUtil.generateEntityId();
+        this.uuid = UUID.randomUUID();
+        this.type = SpigotConversionUtil.fromBukkitEntityType(type);
+        this.location = location;
     }
 
-    /* =========================
-       Spawn
-       ======================== */
-    public void spawn(Player player) {
-        WrapperPlayServerSpawnEntity spawn = new WrapperPlayServerSpawnEntity(
-                entityId,
-                type,
-                location.getX(),
-                location.getY(),
-                location.getZ(),
-                location.getYaw(),
-                location.getPitch()
-        );
-
-        spawn.sendPacket(player);
-        sendMetadata(player);
+    public Location getLocation() {
+        return location;
     }
 
-    /* =========================
-       Metadata
-       ========================= */
-    protected void sendMetadata(Player player) {
-        WrapperPlayServerUpdateEntityMetadata packet =
-                new WrapperPlayServerUpdateEntityMetadata(entityId, metadata);
-
-        packet.sendPacket(player);
-    }
-
-    protected void setMetadata(int index, EntityDataTypes type, Object value) {
-        metadata.removeIf(data -> data.getIndex() == index);
-        metadata.add(new EntityData(index, type, value));
-    }
-
-    /* =========================
-       Despawn
-       ========================= */
-    public void despawn(Player player) {
-        WrapperPlayServerDestroyEntities packet =
-                new WrapperPlayServerDestroyEntities(entityId);
-
-        packet.sendPacket(player);
-    }
-
-    /* =========================
-       Getter (QUAN TRỌNG)
-       ========================= */
     public int getEntityId() {
         return entityId;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public List<EntityData<?>> getEntityData() {
+        return new ArrayList<>();
+    }
+
+    public void spawn(Player viewer) {
+        WrapperPlayServerSpawnEntity packet = new WrapperPlayServerSpawnEntity(
+                this.entityId,
+                this.uuid,
+                this.type,
+                SpigotConversionUtil.fromBukkitLocation(this.location),
+                this.location.getYaw(),
+                0,
+                null
+        );
+        this.sendPacket(packet, viewer);
+    }
+
+    public void update(Player viewer) {
+        WrapperPlayServerEntityMetadata packet = new WrapperPlayServerEntityMetadata(
+                this.entityId,
+                this.getEntityData()
+        );
+        this.sendPacket(packet, viewer);
+    }
+
+    public void despawn(Player viewer) {
+        WrapperPlayServerDestroyEntities packet = new WrapperPlayServerDestroyEntities(
+                this.entityId
+        );
+        this.sendPacket(packet, viewer);
+    }
+
+    public void mount(Entity entity, Player viewer) {
+        WrapperPlayServerSetPassengers packet = new WrapperPlayServerSetPassengers(
+                entity.getEntityId(),
+                new int[] { this.entityId }
+        );
+        this.sendPacket(packet, viewer);
+    }
+
+    private void sendPacket(PacketWrapper<?> packet, Player player) {
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, packet);
     }
 }
